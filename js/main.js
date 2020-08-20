635fd8802166c1e1f244373f696c2032fc5daef4
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-20 12:18:22 -0400

    add labels to on/off switch and hover styles to nav

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@ const bigCirc1 = document.querySelector('.big-circ1');
 const smallCirc2 = document.querySelector('.small-circ2');
 const bigCirc2 = document.querySelector('.big-circ2');
 
+// On/off switch labels
+const emailOn1 = document.querySelector('.email-on1');
+const emailOff1 = document.querySelector('.email-off1');
+const emailOn2 = document.querySelector('.email-on2');
+const emailOff2 = document.querySelector('.email-off2');
+
 // User form variables 
 const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
@@ -51,10 +57,15 @@ send.addEventListener('click', () => {
 bigCirc1.addEventListener('click', () => {
     smallCirc1.classList.toggle('margin-left');
     bigCirc1.classList.toggle('color-change');
+    emailOn1.classList.toggle('displayBlock');
+    emailOff1.classList.toggle('displayNone');
 })
+
 bigCirc2.addEventListener('click', () => {
     smallCirc2.classList.toggle('margin-left');
     bigCirc2.classList.toggle('color-change');
+    emailOn2.classList.toggle('displayBlock');
+    emailOff2.classList.toggle('displayNone');
 })
 
 // Charts
